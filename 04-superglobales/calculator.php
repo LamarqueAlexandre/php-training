e45722a5052470d61c4a34f3259668e43bd44d5e
LamarqueAlexandre <lamarque.alexandre7@gmail.com>
2018-10-30 22:42:19 +0100

    Calculator.php

---
+++ 04-superglobales/calculator.php
@@ -0,0 +1,15 @@
+<h1>Le calculateur</h1> 
+
+    <form method="POST">
+        <div class="form-group">
+            <label for="nombre1">1er nombre</label>
+            <input type="text" class="form-control" name="nombre1" placeholder="1er nombre">
+        </div>
+        <div class="form-group">
+            <label for="nombre2">2eme nombre</label>
+            <input type="text" class="form-control" name="nombre2" placeholder="2eme nombre">
+        </div>
+        <button type="submit" class="btn btn-primary">Calculez</button>
+    </form>
+
+
