f36bf16449d07590f9888533f20e705234dbd0aa
LamarqueAlexandre <lamarque.alexandre7@gmail.com>
2018-10-30 17:47:51 +0100

    Functions exercises

--- 03-fonctions/01-exercice.php
+++ 03-fonctions/01-exercice.php
@@ -1,6 +1,7 @@
 <?php
 
 // 1. Créer une fonction calculant le carré d'un nombre
+echo '<h2>1/ Créer une fonction calculant le carré d\'un nombre</h2>';
 function square($number) {
     return $number * $number;
 }
@@ -10,6 +11,14 @@ echo square(12). '<br>';
 echo square(48). '<br>';
 
 // 2. Créer une fonction calculant l'aire d'un rectangle et une fonction pour celle d'un cercle
+
+echo '<h2>2/ Créer une fonction calculant l\'aire d\'un rectangle et une fonction pour celle d\'un cercle</h2>';
+function rectArea($length, $width) {
+    return $length * $width;
+}
+
+echo rectArea(10, 5). '<br>';
+
 // 3. Créer une fonction calculant le prix TTC d'un prix HT. Nous aurons besoin de 2 paramètres, le prix HT et le taux.
 // 4. Ajouter un 3ème paramètre à cette fonction permettant de l'utiliser dans les 2 sens (HT vers TTC ou TTC vers HT)
 
